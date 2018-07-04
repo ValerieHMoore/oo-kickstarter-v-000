05f4253e38709b5ef91597e548bf089c10b8cfbd
Valerie Moore <periwinkle17@outlook.com>
2018-07-04 23:18:29 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,12 +9,6 @@ class Backer
     @backed_projects = []
   end
  
-  # describe 'Backer - More Advanced #back_project' do
-  # it 'also adds the backer to the project\'s backers array' do
-  #   logan = Backer.new("Logan")
-  #   hoverboard = Project.new("Awesome Hoverboard")
-  #   logan.back_project(hoverboard)
-
   def back_project(project)
     @backed_projects << project
     #@project.backers << backer
