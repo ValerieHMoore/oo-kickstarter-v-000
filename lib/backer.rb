1e36838ea8c2ab9c07c2991e96df328036c50cb9
Valerie Moore <periwinkle17@outlook.com>
2018-07-04 23:10:08 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -14,10 +14,10 @@ class Backer
    new_project = Project.new
    
   end
-player = Player.new
-while player.card_pile.length < 2 do
-    new_card = Card.new
-    player.card_pile.push(new_card.value)
-end
+# player = Player.new
+# while player.card_pile.length < 2 do
+#     new_card = Card.new
+#     player.card_pile.push(new_card.value)
+# end
 
 end
